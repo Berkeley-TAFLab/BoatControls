1ed977dc0eeaacdffcbc4e95fcfe01d25dfa2ac2
Alex Lu <alex.y.lu@berkeley.edu>
2024-07-09 18:56:07 -0700

    Add source code for rf433 module. Should be able to pingpong with a control panel now

--- Servo/ESPConversion/include/constants.h
+++ Servo/ESPConversion/include/constants.h
@@ -4,13 +4,15 @@
 #define SAIL_SERVO_PIN 13 
 #define TAIL_SERVO_PIN 7 //This needs to be redefined  
 
+#define RF_433_TX_PIN 39 //Transceiver pin for 433MHz transceiver module
+#define RF_433_RX_PIN 34 //Receiver pin for 433 MHz receiver module 
+
 #define GPS_RX_PIN 16 //From tx on gps to rx on esp32
 #define GPS_TX_PIN 17 //From Rx on esp32 to tx on gps but you can maybe disregard this entirely
 
 //Static pin definitions. You should try to refrain from changing these 
 #define ENCODER_SDA_PIN 21 //Don't change
 #define ENCODER_SCL_PIN 22 //Don't change
-
 
 //Task priority definitions (These shouldn't really be changed much) 
 #define WATER_DETECTION_PRIO 5
@@ -44,6 +46,8 @@ const float lis3mdl_soft_iron_vals[3][3] = {
 }; 
 
 //Values related to RF 433MHz transmission 
+#define CP_ID 0x00 //Value of the control panel ID
 #define RF_ID 0x01 // This value will change as more devices are added
+
 
 //Debugging variables to disable certain setup tasks or functions
--- Servo/ESPConversion/src/TAF_RF433MHZ.cpp
+++ Servo/ESPConversion/src/TAF_RF433MHZ.cpp
@@ -1,6 +1,46 @@
+/*
+    Source file containing all functions and code related to the 433Mhz radio module
+*/
+//Predefined libraries and headers
+#include <RHReliableDatagram.h>
+#include <RH_ASK.h>
+#include <SPI.h>
 
 //User defined libraries and headers
-#include "TAF_RF433MHZ.h" 
+#include "TAF_RF433MHZ.h"  
+#include "constants.h" 
 
+RH_ASK driver(2000, RF_433_RX_PIN, RF_433_TX_PIN, -1); // bitrate, rx, tx, pttpin
 
+RHReliableDatagram manager(driver, RF_ID); 
 
+void setup_rf433(){
+    if(!manager.init()){
+        Serial.println("Failed to init RF manager class.");
+    }
+} 
+
+uint8_t data[] = "And hello back to you";
+// Dont put this on the stack:
+uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+
+void receive_rf433()
+{
+  if (manager.available())
+  {
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
+    {
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+
+      // Send a reply back to the originator client
+      if (!manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("sendtoWait failed");
+    }
+  }
+}
