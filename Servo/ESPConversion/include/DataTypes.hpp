38612154eb1e2a4939dca3bff457a9c6c91a5e8d
Kieran Pereira <kieranpereira@hotmail.com>
2024-10-12 15:30:18 -0700

    Added datatypes.hpp to improve organisation

---
+++ Servo/ESPConversion/include/DataTypes.hpp
@@ -0,0 +1,17 @@
+// Filename: Datatypes.hpp
+// Author: Kieran Pereira
+// Date: 10/11/2024
+// Description: Header file containing shared data structures used across multiple scripts.
+
+#ifndef DATATYPES_HPP
+#define DATATYPES_HPP
+
+namespace Datatypes {
+    // Structure Definitions
+    struct Coordinate {
+        float latitude;
+        float longitude;
+    };
+}
+
+#endif // DATATYPES_HPP
